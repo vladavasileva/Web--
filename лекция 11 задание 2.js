2a26fa0b663e089f4211e100427388a3c332fac8
vladavasileva <91936629+vladavasileva@users.noreply.github.com>
2021-11-20 17:10:21 +0200

    Add files via upload

---
+++ лекция 11 задание 2.js
@@ -0,0 +1,8 @@
+function random(min, max) {
+    return min + Math.random() * (max - min);
+}
+
+alert( random(1, 7) );
+alert( random(1, 7) );
+alert( random(1, 7) );
+alert( random(1, 7) );
---
+++ лекция 11 задание 3.js
@@ -0,0 +1,4 @@
+let num1 = +prompt("Первое число :", 0);
+let num2 = +prompt("Второе число :", 0);
+
+alert( num1 + num2);
